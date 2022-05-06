51e53569cf51b6046b3076f751e4c1cd673c3de3
Phan Nguyễn Dũng Mạnh (TRN.Fresher) <manh.phan@bnksolution.com>
2022-05-06 16:35:17 +0700

    add file .sh run docker

---
+++ README.md
@@ -0,0 +1,6 @@
+# phpapache
+run php apache
+```
+sudo chmod +x run.sh
+./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker-compose -f docker-compose.yml -f docker-compose.mysql.yml -f docker-compose.phpadmin.yml up -d
